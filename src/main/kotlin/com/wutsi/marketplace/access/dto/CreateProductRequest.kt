0a501c66f89b464c7002286e00d48cec06d249d2
wutsi <herve.tchepannou@wutsi.com>
2022-11-13 12:28:49 -0500

    update scehma

--- src/main/kotlin/com/wutsi/marketplace/access/dto/CreateProductRequest.kt
+++ src/main/kotlin/com/wutsi/marketplace/access/dto/CreateProductRequest.kt
@@ -5,9 +5,9 @@ import kotlin.String
 
 public data class CreateProductRequest(
   public val storeId: Long = 0,
-  public val pictureUrl: String = "",
+  public val pictureUrl: String? = null,
   public val categoryId: Long? = null,
-  public val title: String? = null,
+  public val title: String = "",
   public val summary: String? = null,
   public val price: Long? = null,
 )
