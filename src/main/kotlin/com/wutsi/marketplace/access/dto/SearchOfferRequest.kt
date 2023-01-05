ae299366a787220e9bedbe470789ea3162805d6b
wutsi <herve.tchepannou@wutsi.com>
2023-01-05 01:16:51 -0500

    update schema

--- src/main/kotlin/com/wutsi/marketplace/access/dto/SearchOfferRequest.kt
+++ src/main/kotlin/com/wutsi/marketplace/access/dto/SearchOfferRequest.kt
@@ -5,7 +5,8 @@ import kotlin.Long
 import kotlin.String
 
 public data class SearchOfferRequest(
-  public val storeId: Long = 0,
+  public val storeId: Long? = null,
+  public val productIds: List<Long> = emptyList(),
   public val limit: Int = 100,
   public val offset: Int = 0,
   public val sortBy: String? = null,
